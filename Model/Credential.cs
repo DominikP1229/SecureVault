3937824dafd4cc8873aea7b556a3357d630243b4
Dominik Ptak <88987@student.pb.edu.pl>
2026-03-26 18:15:33 +0100

    Dodaj pliki projektów.

---
+++ Model/Credential.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureVault.Model
+{
+    public class Credential
+    {
+        public Credential()
+        {
+        }
+
+        public string? Title { get; set; }
+        public string? Username { get; set; }
+        public string? Category { get; set; }
+    }
+}
